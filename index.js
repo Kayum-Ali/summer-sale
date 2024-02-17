f2ec92bb93df20fff40ea7f7a9d0c4b2e10ddc2b
Kayum <sisirfahimstudent@gmail.com>
2024-02-17 22:14:43 +0600

    design done

---
+++ index.js
@@ -0,0 +1,8 @@
+const cards = document.querySelectorAll(".card");
+for(const index = 0; index < cards.length; index++){
+    const card = cards[index];
+    card.addEventListener('click', function(){
+        const title = card.querySelector('h3');
+        
+    })
+}
